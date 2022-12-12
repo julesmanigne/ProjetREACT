39bdd7c167e2d46044058bba60a6066fd4588aa5
Jules Manigne-Malan <64989111+jmjmpx@users.noreply.github.com>
2022-12-12 17:21:43 +0100

    update

--- frontend/src/components/Widget5.jsx
+++ frontend/src/components/Widget5.jsx
@@ -1,94 +1,121 @@
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import "../index2.css";
 
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import '../index2.css';
-
-import React, { useState } from 'react'
-import { GiHornedHelm } from 'react-icons/gi'
-import { AiOutlinePlus, AiOutlineClose } from 'react-icons/ai'
+import React, { useState } from "react";
+import { GiHornedHelm } from "react-icons/gi";
+import { AiOutlinePlus, AiOutlineClose } from "react-icons/ai";
 
 function App() {
-  const [tasks, setTasks] = useState([])
-  const [input, setInput] = useState('')
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState("");
 
   // ajouter une tache
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const addTask = {
       id: Math.floor(Math.random() * 1000),
       text: input,
-      completed: false
-    }
-    setTasks([...tasks, addTask])
-    setInput('')
-  }
+      completed: false,
+    };
+    setTasks([...tasks, addTask]);
+    setInput("");
+  };
 
   // enlever
   const deleteTask = (id) => {
-    let filteredTasks = [...tasks].filter((tasks) => tasks.id !== id)
-    setTasks(filteredTasks)
-    console.log('task deleted')
-  }
+    let filteredTasks = [...tasks].filter((tasks) => tasks.id !== id);
+    setTasks(filteredTasks);
+    console.log("task deleted");
+  };
 
   // toggle completed task
   const toggleComplete = (id) => {
     setTasks(
-      tasks.map(task => (
+      tasks.map((task) =>
         task.id === id ? { ...task, completed: !task.completed } : task
-      ))
-    )
-  }
+      )
+    );
+  };
 
-  const date = new Date()
+  const date = new Date();
   // console.log(date)
-  const days = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"]
-  const months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"]
-
+  const days = [
+    "Dimanche",
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi",
+  ];
+  const months = [
+    "Janvier",
+    "Février",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juillet",
+    "Aout",
+    "Septembre",
+    "Octobre",
+    "Novembre",
+    "Décembre",
+  ];
 
   return (
-    <Card sx={{ height: '100%' }}>
-        <div className='app'>
-      <div className="container">
-        <h1> To do List</h1>
+    <Card sx={{ height: "100%" }}>
+      <div className="app">
+        <div className="container">
+          <h1> To do List</h1>
 
-        <div className="date">
-          <p>{days[date.getDay()]}</p>
-          <p>{date.getDate()}</p>
-          <p>{months[date.getMonth()]}</p>
-          <p>{date.getFullYear()}</p>
-        </div>
-
-        <form onSubmit={handleSubmit}>
-          <div className="form-input">
-            <AiOutlinePlus className='icon' />
-            <input
-              value={input}
-              onChange={e => setInput(e.target.value)}
-              placeholder='Ajouter une tâche'
-              type="text" />
+          <div className="date">
+            <p>{days[date.getDay()]}</p>
+            <p>{date.getDate()}</p>
+            <p>{months[date.getMonth()]}</p>
+            <p>{date.getFullYear()}</p>
           </div>
-        </form>
 
-        <div>
-          {tasks.map(task => (
-            <div className={`task-row ${task.completed ? 'completed' : ''}`} key={task.id} onDoubleClick={() => toggleComplete(task.id)} >
-              <p>{task.text} </p>
-              <AiOutlineClose onClick={() => deleteTask(task.id)} className='icon' />
+          <form onSubmit={handleSubmit}>
+            <div className="form-input">
+              <AiOutlinePlus className="icon" />
+              <input
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                placeholder="Ajouter une tâche"
+                type="text"
+              />
             </div>
-          ))}
-        </div>
+          </form>
 
-          <p className='length'>{(tasks < 1) ? 'Aucune tâche' : `Tâche: ${tasks.length}`}</p>
+          <div>
+            {tasks.map((task) => (
+              <div
+                className={`task-row ${task.completed ? "completed" : ""}`}
+                key={task.id}
+                onDoubleClick={() => toggleComplete(task.id)}
+              >
+                <p>{task.text} </p>
+                <AiOutlineClose
+                  onClick={() => deleteTask(task.id)}
+                  className="icon"
+                />
+              </div>
+            ))}
+          </div>
+
+          <p className="length">
+            {tasks < 1 ? "Aucune tâche" : `Tâche: ${tasks.length}`}
+          </p>
+        </div>
       </div>
-    </div>
-     </Card>
-    
+    </Card>
   );
 }
 
 export default App;
-
--- frontend/src/components/WidgetAlarm.jsx
+++ frontend/src/components/WidgetAlarm.jsx
@@ -1,23 +1,20 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
 
-
-  export default function WidgetAlarm() {
-        return (
-      <>
-          <Card sx={{ height: '100%' }}>
-            <CardMedia />
-            <CardContent>
-
-          
-          </CardContent><CardActions>
-              <Button size="small">Get Alarms</Button>
-            </CardActions>
-     </Card>
-     </>
-    );
-  }
+export default function WidgetAlarm() {
+  return (
+    <>
+      <Card sx={{ height: "100%" }}>
+        <CardMedia />
+        <CardContent></CardContent>
+        <CardActions>
+          <Button size="small">Get Alarms</Button>
+        </CardActions>
+      </Card>
+    </>
+  );
+}
