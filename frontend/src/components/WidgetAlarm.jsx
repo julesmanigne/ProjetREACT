f0def2f463f9362146887bb5a72fe84b7302c9e5
Jules Manigne-Malan <64989111+jmjmpx@users.noreply.github.com>
2022-12-16 15:32:54 +0100

    chart

--- frontend/src/components/WidgetAlarm.jsx
+++ frontend/src/components/WidgetAlarm.jsx
@@ -8,31 +8,28 @@ import Button from "@mui/material/Button";
 import axios from "axios";
 import Typography from "@mui/material/Typography";
 import { Grid, Link, List } from "@mui/material";
-import ListItemText from '@mui/material/ListItemText';
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import EditIcon from '@mui/icons-material/Edit';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
+import ListItemText from "@mui/material/ListItemText";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import EditIcon from "@mui/icons-material/Edit";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
-import AddAlarm from '../pages/AddAlarm';
-import Collapsible from '../pages/Collapsible';
-import ModifyAlarm from '../pages/ModifyAlarm';
-import TextField from '@mui/material/TextField';
-import '../App.css';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import dayjs from 'dayjs';
-import { FormControl } from '@mui/material';
+import AddAlarm from "../pages/AddAlarm";
+import Collapsible from "../pages/Collapsible";
+import ModifyAlarm from "../pages/ModifyAlarm";
+import TextField from "@mui/material/TextField";
+import "../App.css";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
+import { FormControl } from "@mui/material";
 
-
-const label = { inputProps: { 'aria-label': 'Switch demo' } };
-
+const label = { inputProps: { "aria-label": "Switch demo" } };
 
 export default function WidgetAlarm() {
-
   const [value, setValue] = React.useState(dayjs());
-  const [name, setName] = useState('')
-  const [alarms, setAlarms] = useState([])
+  const [name, setName] = useState("");
+  const [alarms, setAlarms] = useState([]);
 
   const deleteHandler = async (id) => {
     if (window.confirm("Delete?")) {
@@ -43,28 +40,25 @@ export default function WidgetAlarm() {
   const createHandler = async (id) => {
     if (window.confirm("Add?")) {
       const payload = {
-        "label": name,
-        "time": value,
-        "state": true
-      }
+        label: name,
+        time: value,
+        state: true,
+      };
       const { data } = await axios.post(`/alarms/alarm`, payload);
     }
   };
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     if (name && value) {
-      console.log('value is:', name, value);
+      console.log("value is:", name, value);
     }
-  }
-
-
+  };
 
   const fetchAlarms = async () => {
     const { data } = await axios.get("/alarms");
 
     setAlarms(data);
-
   };
 
   console.log(alarms);
@@ -73,10 +67,8 @@ export default function WidgetAlarm() {
     fetchAlarms();
   }, []);
 
-
   return (
     <>
-
       <Card sx={{ height: "100%" }}>
         <CardMedia />
         <CardContent>
@@ -86,37 +78,63 @@ export default function WidgetAlarm() {
           <Typography variant="body2" color="text.secondary">
             {alarms.data?.map((data, id) => {
               let options = {
-                weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric',
+                weekday: "long",
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+                hour: "numeric",
+                minute: "numeric",
+                second: "numeric",
                 hour12: false,
               };
 
               const d1 = new Date(data.time);
-              const d2 = new Intl.DateTimeFormat('default', options).format(d1);
+              const d2 = new Intl.DateTimeFormat("default", options).format(d1);
 
-              return <div key={id}>
-                <br />
-                <Grid container spacing={2}>
-                  <List sx={{ width: '100%' }}>
-                    <ListItemText sx={{ marginLeft: 2 }} primary={data.label} primaryTypographyProps={{
-                      fontSize: 25,
-                      fontWeight: 'bold',
-                      letterSpacing: 0,
-                    }}> </ListItemText>
-                  </List>
-                  <Grid item xs={2} md={4}>
-                    <h4>{d2}</h4>
+              return (
+                <div key={id}>
+                  <br />
+                  <Grid container spacing={2}>
+                    <List sx={{ width: "100%" }}>
+                      <ListItemText
+                        sx={{ marginLeft: 2 }}
+                        primary={data.label}
+                        primaryTypographyProps={{
+                          fontSize: 25,
+                          fontWeight: "bold",
+                          letterSpacing: 0,
+                        }}
+                      >
+                        {" "}
+                      </ListItemText>
+                    </List>
+                    <Grid item xs={2} md={4}>
+                      <h4>{d2}</h4>
+                    </Grid>
+                    <Grid item xs={2} md={4}>
+                      {data.state ? "ON" : "OFF"}
+                    </Grid>
+                    <Grid item xs={6} md={4}>
+                      <Button
+                        variant="outlined"
+                        size="small"
+                        style={{ marginRight: 10 }}
+                        onClick={() => deleteHandler(alarms.data[id]._id)}
+                      >
+                        {" "}
+                        <DeleteForeverIcon />
+                      </Button>
+                      <Button
+                        href={`/alarm/${alarms.data[id]._id}`}
+                        variant="outlined"
+                        size="small"
+                      >
+                        <EditIcon />
+                      </Button>
+                    </Grid>
                   </Grid>
-                  <Grid item xs={2} md={4}>
-                    {data.state ? "ON" : "OFF"}
-                  </Grid>
-                  <Grid item xs={6} md={4}>
-                    <Button variant="outlined" size="small" style={{ marginRight: 10 }} onClick={() => deleteHandler(alarms.data[id]._id)}>  <DeleteForeverIcon /></Button>
-                    <Button href={`/alarm/${alarms.data[id]._id}`} variant="outlined" size="small"><EditIcon /></Button>
-                  </Grid>
-
-
-                </Grid>
-              </div>
+                </div>
+              );
             })}
           </Typography>
         </CardContent>
@@ -125,10 +143,17 @@ export default function WidgetAlarm() {
             <Grid item xs={2} md={12}>
               <Collapsible label="Add">
                 <form onSubmit={handleSubmit}>
-                  <TextField onChange={(e) => setName(e.target.value)} id="outlined-basic" label="Name" variant="outlined" size="small" style={{ width: '100%', marginBottom: 30 }} />
+                  <TextField
+                    onChange={(e) => setName(e.target.value)}
+                    id="outlined-basic"
+                    label="Name"
+                    variant="outlined"
+                    size="small"
+                    style={{ width: "100%", marginBottom: 30 }}
+                  />
                   <LocalizationProvider dateAdapter={AdapterDayjs}>
                     <DateTimePicker
-                      style={{ width: '100%', height: 40 }}
+                      style={{ width: "100%", height: 40 }}
                       renderInput={(props) => <TextField {...props} />}
                       label="Time and Date of alarm"
                       value={value}
@@ -137,14 +162,20 @@ export default function WidgetAlarm() {
                       }}
                     />
                   </LocalizationProvider>
-                  <Button type="submit" style={{ width: '100%', height: 40 }} size="small" onClick={() => createHandler()}><AddCircleIcon /></Button>
+                  <Button
+                    type="submit"
+                    style={{ width: "100%", height: 40 }}
+                    size="small"
+                    onClick={() => createHandler()}
+                  >
+                    <AddCircleIcon />
+                  </Button>
                 </form>
               </Collapsible>
             </Grid>
           </Grid>
         </CardActions>
       </Card>
-
     </>
   );
 }
