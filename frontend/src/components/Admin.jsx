defed097ce1e7fe9f57b121b1a7c0aadc933258e
DylanLuna <galibander@hotmail.com>
2022-12-19 18:56:42 +0100

    admin added

--- frontend/src/components/Admin.jsx
+++ frontend/src/components/Admin.jsx
@@ -1,65 +1,105 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import { DataGrid } from '@mui/x-data-grid';
 import axios from "axios";
+import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { Grid, List } from "@mui/material";
 import ListItemText from "@mui/material/ListItemText";
-import Collapsible from "../pages/Collapsible";
-import Collapsible2 from "../pages/Collapsible2";
-import TextField from "@mui/material/TextField";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import dayjs from "dayjs";
-import Switch from '@mui/material/Switch';
+import "../App.css";
 import Box from '@mui/material/Box';
-import { DataGrid } from '@mui/x-data-grid';
-
 import "../App.css";
 
 const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 90,
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-    },
-  ];
-  
+    { field: 'label', headerName: 'Label', width: 300 },
+    { field: 'status', headerName: 'Status', width: 600 },
+    { field: 'time', headerName: 'Date and Time', width: 300 },
+    { field: '_id', headerName: 'ID' },
+  ]
+ 
   const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, label: 'Snow', status: 'Jon', time: 35 },
+    { id: 2, label: 'Lannister', status: 'Cersei', time: 42 },
+    { id: 3, label: 'Lannister', status: 'Jaime', time: 45 },
+  
   ];
+ 
 
-const Admin = () => (
-    <div style={{ height: 400, width: '100%' }}>
-    <DataGrid
-      rows={rows}
-      columns={columns}
-      pageSize={5}
-      rowsPerPageOptions={[5]}
-      checkboxSelection
-    />
-  </div>
-);
+export default function Admin(){
 
-export default Admin;
+    const [alarms, setAlarms] = useState([]);   
+
+    const fetchAlarms = async () => {
+        const { data } = await axios.get("/alarms");
+    
+        setAlarms(data);
+      };
+    
+      console.log(alarms);
+    
+      useEffect(() => {
+        fetchAlarms();
+      }, []);
+
+   return(
+
+        <div style={{ height: 400, width: '100%', backgroundColor:"#fff"}}>
+         <Typography variant="body2" color="text.secondary">
+            {alarms.data?.map((data, id) => {
+              let options = {
+                weekday: "long",
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+                hour: "numeric",
+                minute: "numeric",
+                second: "numeric",
+                hour12: false,
+              };
+
+              const d1 = new Date(data.time);
+              const d2 = new Intl.DateTimeFormat("default", options).format(d1);
+
+              return (
+                <div key={id}>
+                  <Box sx={{}}>
+                    <Grid item xs={18}>
+                      <List sx={{ display: "flex", justifyContent: "center", border: "1px solid rgb(95, 122, 227);", marginBottom: 2, borderRadius: 5, paddingLeft: 5 }}>
+                        <ListItemText
+                          sx={{}}
+                          primary={data.label}
+                          secondary={d2}
+                          primaryTypographyProps={{
+                            fontFamily: "Ubuntu",
+                            fontSize: 30,
+                            fontWeight: "bold",
+                            letterSpacing: 1,
+                          }}
+                        >
+                          {" "}
+                        </ListItemText>
+                        <Grid sx={{ textAlign: 'center', display: "flex" }} item xs={2} md={12}>
+                          <ListItemText
+                            sx={{ margin: 'auto' }}
+                            primary={data.status ? "ON" : "OFF"}
+                            primaryTypographyProps={{
+                              fontFamily: "Ubuntu",
+                              fontSize: 30,
+                              fontStyle: 'italic',
+                              fontWeight: 1000,
+                              letterSpacing: 0,
+                            }}
+                          >
+                            {" "}
+                          </ListItemText>
+                        </Grid>
+                      </List>
+                    </Grid>
+                  </Box>
+                </div>
+              );
+            })}
+          </Typography>
+       </div>
+        )
+  }
