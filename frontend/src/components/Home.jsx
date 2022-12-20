d3037bb314acb0cb2a31fe18783ba06fc164593e
Jules Manigne-Malan <64989111+jmjmpx@users.noreply.github.com>
2022-12-20 11:59:43 +0100

    up

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { Typography, AppBar, CssBaseline, Grid, Toolbar } from "@mui/material";
-import DashboardIcon from "@mui/icons-material/Dashboard";
+import { CssBaseline, Grid } from "@mui/material";
 import WidgetCard from "./WidgetCard";
 import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
 import WidgetAlarm from "./WidgetAlarm";
 import WidgetWeather from "./WidgetWeather";
 import Widget4 from "./Widget4";
@@ -17,7 +14,7 @@ import ModifyAlarm from "../pages/ModifyAlarm";
 
 export { AddAlarm, DeleteAlarm, ModifyAlarm };
 
-const darkTheme = createTheme({
+const lightheme = createTheme({
   palette: {
     mode: "light",
     background: {
@@ -27,46 +24,45 @@ const darkTheme = createTheme({
 });
 
 const Home = () => {
-
   return (
     <>
-      <ThemeProvider theme={darkTheme}>
+      <ThemeProvider theme={lightheme}>
         <CssBaseline />
       </ThemeProvider>
       <main>
-          <Box sx={{ bgcolor: "#F8FAFB", height: "100%", padding: 2, }}>
-            <Box
-              sx={{
-                flexDirection: "row",
-                flexWrap: "wrap",
-                display: "flex",
-                justifyContent: "space-around",
-                p: 5,
-                m: "auto",
-              }}
-            >
-              <Grid container spacing={2} columns={12}>
-                <Grid item lg={6} md={6} sm={12} xs={12}>
-                  <WidgetCard></WidgetCard>
-                </Grid>
-                <Grid item lg={6} md={6} sm={12} xs={12}>
-                  <WidgetAlarm></WidgetAlarm>
-                </Grid>
-                <Grid item lg={4} md={4} sm={12} xs={12}>
-                  <WidgetWeather></WidgetWeather>
-                </Grid>
-                <Grid item lg={4} md={4} sm={12} xs={12}>
-                  <Widget4></Widget4>
-                </Grid>
-                <Grid item lg={4} md={4}sm={12} xs={12}>
-                  <Widget5></Widget5>
-                </Grid>
-                <Grid item lg={12} md={12}sm={12} xs={12}>
-                  <Widget6></Widget6>
-                </Grid>
+        <Box sx={{ bgcolor: "#F8FAFB", height: "100%", padding: 2 }}>
+          <Box
+            sx={{
+              flexDirection: "row",
+              flexWrap: "wrap",
+              display: "flex",
+              justifyContent: "space-around",
+              p: 5,
+              m: "auto",
+            }}
+          >
+            <Grid container spacing={2} columns={12}>
+              <Grid item lg={6} md={6} sm={12} xs={12}>
+                <WidgetCard></WidgetCard>
               </Grid>
-            </Box>
+              <Grid item lg={6} md={6} sm={12} xs={12}>
+                <WidgetAlarm></WidgetAlarm>
+              </Grid>
+              <Grid item lg={4} md={4} sm={12} xs={12}>
+                <WidgetWeather></WidgetWeather>
+              </Grid>
+              <Grid item lg={4} md={4} sm={12} xs={12}>
+                <Widget4></Widget4>
+              </Grid>
+              <Grid item lg={4} md={4} sm={12} xs={12}>
+                <Widget5></Widget5>
+              </Grid>
+              <Grid item lg={12} md={12} sm={12} xs={12}>
+                <Widget6></Widget6>
+              </Grid>
+            </Grid>
           </Box>
+        </Box>
       </main>
     </>
   );
